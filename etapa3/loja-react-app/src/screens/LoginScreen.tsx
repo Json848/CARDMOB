3667f1f7b361369b6f0af6de11c6998907e9ed12
Json848 <yandalbelo@gmail.com>
2025-09-02 08:04:22 -0300

    incluir exemplo e icone exemplo e ícone no tabnavigator

--- etapa3/loja-react-app/src/screens/LoginScreen.tsx
+++ etapa3/loja-react-app/src/screens/LoginScreen.tsx
@@ -1,5 +1,12 @@
 import React, { use, useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Text,
+  SafeAreaView,
+} from 'react-native';
 import { fakeLogin } from '../services/authServices';
 import { useAuth } from '../context/AuthContext';
 
@@ -7,13 +14,13 @@ export default function LoginScreen({ navigation }: any) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const {login} = useAuth()
+  const { login } = useAuth();
 
   const handleLogin = async () => {
     try {
       // Lógica de login / conexão com backend.
-      const token = await fakeLogin(email, password)
-      login(token)
+      const token = await fakeLogin(email, password);
+      login(token);
       console.log('Login ok');
     } catch (err: any) {
       setError(err);
@@ -21,28 +28,30 @@ export default function LoginScreen({ navigation }: any) {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>Email:</Text>
-      <TextInput
-        style={styles.input}
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-      />
-      <Text>Senha:</Text>
-      <TextInput
-        style={styles.input}
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
-      <Button title="Entrar" onPress={handleLogin} />
-      <Button
-        title="Registrar"
-        onPress={() => navigation.navigate('Register')}
-      />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Email:</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+        />
+        <Text>Senha:</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
+        <Button title="Entrar" onPress={handleLogin} />
+        <Button
+          title="Registrar"
+          onPress={() => navigation.navigate('Register')}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
