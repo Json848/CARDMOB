051a1f72b44d130c6ea45519e91af2660097003a
Json848 <yandalbelo@gmail.com>
2025-10-07 07:37:58 -0300

    carrinho de compras mínimo

--- etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
+++ etapa3/loja-react-app/src/screens/cart/CartScreen.tsx
@@ -1,16 +1,29 @@
-import React, {useContext} from "react";
-import { View, FlatList, StyleSheet, Text, Button } from "react-native";
-export default function CartScreen() {
+import React, { useContext } from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+import CartItem from './CartItem';
+
+const CartScreen = ({ navigation }: any) => {
+  const renderItem = ({ item }: any) => <CartItem item={item} />;
+
+  const cartTemp = [
+    {
+      id: 1,
+      quantity: 2,
+      price: 8.5,
+      name: 'Brigadeiro promoção especial',
+      image: 'http://10.81.205.50:5000/uploads/brigadeiro-matcha-white.png',
+    },
+  ];
+
   return (
-    <View style={styles.container}>
-      <Text>Tela do Carrinho</Text>
+    <View>
+      <Text>Carrinho de compras</Text>
+      <FlatList
+        data={cartTemp}
+        renderItem={renderItem}
+        keyExtractor={(item: any) => item.id.toString()}
+      />
     </View>
   );
-}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+};
